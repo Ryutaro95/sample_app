3dee26f3f9c89d79fab73c6964671713134571aa
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-12 21:50:26 +0900

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # User モデルの中では、右の式の self を省略可能
+  # 保存する直前に実行され、userオブジェクトの email をすべて小文字に変換する
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  # カッコを使った validates メソッド
+  # validates(:name, presence: true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
