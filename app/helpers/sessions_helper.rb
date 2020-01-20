e62e38cda4c871a9909e721253fa3d2adc2e05b2
Ryutaro95 <ryutaro9595@gmail.com>
2020-01-20 21:17:55 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    # cookies に 有効期限 20年、ユーザーIDが暗号化され保存される
+    cookies.permanent.signed[:user_id] = user.id
+    # cookies の remember_token に有効期限 20年で保存される
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -19,6 +34,20 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   # User モデルの中では、右の式の self を省略可能
   # 保存する直前に実行され、userオブジェクトの email をすべて小文字に変換する
   before_save { email.downcase! }
@@ -15,10 +17,37 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
 
+  class << self
   # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    # selfと付けると、remember_token というローカル変数が作成されない
+    self.remember_token = User.new_token
+
+    # update_attributeはバリデーションを素通りさせて、記憶ダイジェストを更新する
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したら true を返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄
+  def forget
+    update_attribute(:remember_token, nil)
   end
 end
